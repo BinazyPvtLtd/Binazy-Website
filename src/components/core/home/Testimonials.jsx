c96d5716a9d6f460f8e8b0114857f526f98e4d91
ekta-developer <eekta.srivastava2000@gmail.com>
2026-07-18 17:15:58 +0530

    make it fix error files
    make it fix error files

--- src/components/core/home/Testimonials.jsx
+++ src/components/core/home/Testimonials.jsx
@@ -2,12 +2,12 @@ import { useState } from "react";
 import { ChevronLeft, ChevronRight, Star } from "lucide-react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { A11y, Autoplay } from "swiper/modules";
-import logo1 from "@/assets/logo1.png";
-import logo2 from "@/assets/logo2.png";
-import logo3 from "@/assets/logo3.png";
-import logo4 from "@/assets/logo4.png";
-import logo5 from "@/assets/logo5.png";
-import logo6 from "@/assets/logo6.png";
+import logo1 from "../../../assets/logo1.png";
+import logo2 from "../../../assets/logo1.png";
+import logo3 from "../../../assets/logo1.png";
+import logo4 from "../../../assets/logo1.png";
+import logo5 from "../../../assets/logo1.png";
+import logo6 from "../../../assets/logo1.png";
 
 import "swiper/css";
 
