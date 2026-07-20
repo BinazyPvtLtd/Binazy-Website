917defc8f32be1ab2b93ecb94fbcbd02e6aee502
ekta-developer <eekta.srivastava2000@gmail.com>
2026-07-20 14:21:38 +0530

    added dist manually
    added dist manually

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import { fileURLToPath } from "url";
-import sitemap from "vite-plugin-sitemap";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -19,18 +18,20 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+
   build: {
     assetsInlineLimit: 4096,
     chunkSizeWarningLimit: 600,
     rollupOptions: {
       output: {
-        manualChunks: (id) => {
+        manualChunks(id) {
           if (
             id.includes("node_modules/react/") ||
             id.includes("node_modules/react-dom/")
           ) {
             return "vendor";
           }
+
           if (id.includes("node_modules/react-router")) {
             return "router";
           }
